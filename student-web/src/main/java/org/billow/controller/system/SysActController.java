d2f3e629ea90549c1d0ec006ece7d73a7bcdbddd
Xiao-Y <lyongtao123@126.com>
2017-06-11 20:58:34 +0800

    添加新的流程图

--- student-web/src/main/java/org/billow/controller/system/SysActController.java
+++ student-web/src/main/java/org/billow/controller/system/SysActController.java
@@ -243,8 +243,10 @@ public class SysActController {
 			DeploymentBuilder builder = repositoryService.createDeployment();
 			// 设置发布信息
 			builder.name("请假流程")// 添加部署规则的显示别名
-					.addClasspathResource("diagrams/QingJiaModel.bpmn20.xml")// 添加规则文件
-					.addClasspathResource("diagrams/QingJiaModel.png");// 添加规则图片 不添加会自动产生一个图片不推荐
+					// .addClasspathResource("diagrams/QingJiaModel.bpmn20.xml")// 添加规则文件
+					// .addClasspathResource("diagrams/QingJiaModel.png");// 添加规则图片 不添加会自动产生一个图片不推荐
+					.addClasspathResource("diagrams/leave/leave.bpmn")// 添加规则文件
+					.addClasspathResource("diagrams/leave/leave.png");// 添加规则图片 不添加会自动产生一个图片不推荐
 			// 完成发布
 			builder.deploy();
 			json.setSuccess(true);
