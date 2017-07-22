ef443e9b9d6168b066f64d291468a707eb0ea950
Xiao-Y <lyongtao123@126.com>
2017-07-22 11:10:27 +0800

    同步菜单sql

--- student-web/src/main/webapp/data/t_menu.sql
+++ student-web/src/main/webapp/data/t_menu.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50614
 File Encoding         : 65001
 
-Date: 2017-07-07 09:08:40
+Date: 2017-07-22 09:37:50
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,7 +29,7 @@ CREATE TABLE `t_menu` (
   `validind` tinyint(1) DEFAULT NULL,
   `displayno` double(11,3) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_menu
@@ -40,12 +40,17 @@ INSERT INTO `t_menu` VALUES ('23', '辅助管理', 'fa-stop-circle', '0', null, 
 INSERT INTO `t_menu` VALUES ('24', '图标查看', 'fa-stop-circle', null, 'http://fontawesome.io/icons/', '23', '1', '20.000');
 INSERT INTO `t_menu` VALUES ('25', '模块依赖', 'fa-stop-circle', null, '/assManage/viewDependence', '23', '1', '16.000');
 INSERT INTO `t_menu` VALUES ('32', '数据字典', 'fa-tasks', null, '/sysDictionary/findDictionary', '19', '1', '4.000');
-INSERT INTO `t_menu` VALUES ('37', 'Activiti', 'fa-anchor', '1', '', '0', '1', '1.000');
+INSERT INTO `t_menu` VALUES ('37', 'Activiti', 'fa-anchor', '0', '', '0', '1', '1.000');
 INSERT INTO `t_menu` VALUES ('38', '流程部署', 'fa-anchor', '0', '/sysAct/findActModel', '37', '1', '1.000');
 INSERT INTO `t_menu` VALUES ('39', '请假申请', 'fa-cogs', '0', '/applyLeave/findLeaveList', '37', '1', '2.000');
 INSERT INTO `t_menu` VALUES ('40', '请假审批', 'fa-cogs', '0', '/approvalLeave/findApprovalLeave', '37', '1', '3.000');
 INSERT INTO `t_menu` VALUES ('41', '请假申请-外置', 'fa-cogs', '0', '/formkey/applyLeave/findLeaveList', '37', '1', '4.000');
 INSERT INTO `t_menu` VALUES ('42', '请假审批-外置', 'fa-cogs', '0', '/formkey/approvalLeave/findApprovalLeave', '37', '1', '5.000');
-INSERT INTO `t_menu` VALUES ('43', '自动任务', 'fa-tasks', '0', '', '0', '1', '2.000');
+INSERT INTO `t_menu` VALUES ('43', '自动任务', 'fa-tasks', '0', '', '0', '1', '8.000');
 INSERT INTO `t_menu` VALUES ('44', '自动任务', 'fa-tasks', '0', '/sysAutoTask/findAutoTask', '43', '1', '1.000');
 INSERT INTO `t_menu` VALUES ('45', '流程模板', 'fa-anchor', '0', '/sysAct/findActModel', '37', '1', '1.500');
+INSERT INTO `t_menu` VALUES ('46', 'ActiveMQ', 'fa-anchor', '0', '', '0', '1', '2.000');
+INSERT INTO `t_menu` VALUES ('47', 'Queue', '', '0', '/activeMQ/index/queue', '46', '1', '1.000');
+INSERT INTO `t_menu` VALUES ('48', 'Topic', '', '0', '/activeMQ/index/topic', '46', '1', '2.000');
+INSERT INTO `t_menu` VALUES ('49', 'Dubbo', 'fa-anchor', '1', '', '0', '1', '4.000');
+INSERT INTO `t_menu` VALUES ('50', 'dubbo调用', 'fa-anchor', '0', '/dubboController/index', '49', '1', '1.000');
