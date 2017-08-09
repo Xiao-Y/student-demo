5a696a08bd151f90f7ebce3492e7b90d881eb2d6
Xiao-Y <lyongtao123@126.com>
2017-08-09 13:13:47 +0800

    修改eureka的服务端和客户端

--- spring-eureka-client/src/main/java/org/billow/controller/Controller.java
+++ spring-eureka-client/src/main/java/org/billow/controller/Controller.java
@@ -7,13 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@Value("${words}")
-	String words;
+    @Value("${words}")
+    String words;
 
-	@RequestMapping("/")
-	public String getWord() {
-		String[] wordArray = words.split(",");
-		int i = (int) Math.round(Math.random() * (wordArray.length - 1));
-		return wordArray[i];
-	}
+    @RequestMapping("/getWord")
+    public String getWord() {
+        String[] wordArray = words.split(",");
+        int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+        return wordArray[i];
+    }
+
+    @RequestMapping("/info")
+    public String home() {
+        return "Hello world";
+    }
 }
