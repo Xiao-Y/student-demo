9e8061c34d6bfd77af6c013f400c8548f2a52aeb
Xiao-Y <lyongtao123@126.com>
2017-08-10 12:53:40 +0800

    服务与服务的通讯ribbon+restTemplate

---
+++ spring-eureka-ribbon/src/main/java/org/biilow/ribbon/service/HelloService.java
@@ -0,0 +1,22 @@
+package org.biilow.ribbon.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Administrator on 2017/8/10.
+ */
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        //spring-eureka-client
+        return restTemplate.getForObject("http://spring-eureka-client/hi?name=" + name, String.class);
+    }
+
+}
+
