a60f57aa417334bcf554d2db5f4eb159c07d5f69
Xiao-Y <lyongtao123@126.com>
2017-08-15 12:30:28 +0800

    分布式配置中心(Spring Cloud Config)

---
+++ spring-cloud-config-client/src/main/java/org/billow/configClient/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package org.billow.configClient;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ spring-cloud-config-client/src/main/java/org/billow/configClient/controller/TestController.java
@@ -0,0 +1,17 @@
+package org.billow.configClient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${mysqldb.datasource.url}")
+    private String foo;
+
+    @RequestMapping("/foo")
+    public String foo() {
+        return foo;
+    }
+}
